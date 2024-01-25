b7cd5e20fb2dedd18ed9e6a17495080e8a46c948
Dima <dimasalabay0711@gmail.com>
2024-01-25 22:56:14 +0200

    rewrote on Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
 import Feedback from './Feedback/Feedback';
 
-// import menuItems from '../data/data';
-
 function App() {
   return (
     <div className="app">
-      {/* <MainMenu items={menuItems} />
-      <ToggleButton text="Click me" type="button" />
-      <Vote /> */}
       <Feedback />
     </div>
   );
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,74 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
+const optionsFeedback = ['good', 'neutral', 'bad'];
+const Feedback = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const leaveFeedback = optionsName => {
+    setFeedback(prevFeedback => {
+      return {
+        ...prevFeedback,
+        [optionsName]: prevFeedback[optionsName] + 1,
+      };
+    });
+  };
+
+  const countTotalFeedback = () => {
+    const values = Object.values(feedback);
+    const total = values.reduce((acum, value) => acum + value, 0);
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = feedback;
+    if (!good) {
+      return 0;
+    }
+    const pstvFeedbackPercentage = Number(
+      ((good / countTotalFeedback()) * 100).toFixed(2)
+    );
+    return pstvFeedbackPercentage;
+  };
+
+  const { good, neutral, bad } = feedback;
+  const total = countTotalFeedback();
+  const pstvFeedbackPercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={optionsFeedback}
+          onLeaveFeedback={leaveFeedback}
+        />
+      </Section>
+      {total ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={pstvFeedbackPercentage}
+          />
+        </Section>
+      ) : (
+        <Notification message="There is no feedback"></Notification>
+      )}
+    </>
+  );
+};
+
+/*
 class Feedback extends Component {
   static optionsFeedback = ['good', 'neutral', 'bad'];
 
@@ -46,40 +110,30 @@ class Feedback extends Component {
     const total = this.countTotalFeedback();
     const pstvFeedbackPercentage = this.countPositiveFeedbackPercentage();
 
-    if (total === 0) {
-      return (
-        <>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={Feedback.optionsFeedback}
-              onLeaveFeedback={this.leaveFeedback}
-            />
-          </Section>
-          <Notification message="There is no feedback"></Notification>
-        </>
-      );
-    }
-
     return (
-      <div>
+      <>
         <Section title="Please leave feedback">
           <FeedbackOptions
             options={Feedback.optionsFeedback}
             onLeaveFeedback={this.leaveFeedback}
           />
         </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={pstvFeedbackPercentage}
-          />
-        </Section>
-      </div>
+        {total ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={pstvFeedbackPercentage}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </>
     );
   }
-}
+} */
 
 export default Feedback;
